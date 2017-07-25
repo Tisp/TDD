a5897a459674dc6ee27fbaada0aea2b32421a10b
thiago <thiago@somarmeteorologia.com.br>
2017-07-25 11:19:27 -0300

    finish chapter 6

--- tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
+++ tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
@@ -42,4 +42,21 @@ class CarrinhoDeComprasTest extends TestCase
 
         $this->assertEquals(1500.00, $valor, null, 0,00001);        
     }
+
+    public function testListaDeProdutos()
+    {
+        $lista = (new CarrinhoDeCompras())
+                 ->adiciona(new Produto("Jogo de Jantar", 200.00, 1))
+                 ->adiciona(new Produto("Jogo de Pratos", 200.00, 1));
+
+        //Testa total na lista
+        $this->assertEquals(2, count($lista->getProdutos()));
+
+        //Testa valor unitario do primeiro produto
+        $this->assertEquals(200.0, $lista->getProdutos()[0]->getValorUnitario());
+
+        //Testa valor unitario do segundo produto
+        $this->assertEquals(200.0, $lista->getProdutos()[1]->getValorUnitario());
+
+    }
 }
